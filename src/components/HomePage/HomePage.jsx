1fe552e32234361217f010cc0e121e558d11b0d2
Ostap21 <57452764+Ostap21@users.noreply.github.com>
2021-04-18 20:07:53 +0300

    add modal window to edit

--- src/components/HomePage/HomePage.jsx
+++ src/components/HomePage/HomePage.jsx
@@ -1,8 +1,15 @@
 import React from 'react';
 import './HomePage.scss';
+import { Paper, Container, Box } from '@material-ui/core';
 
 export const HomePage = () => (
-  <div>
-    Welcome to our Product-App web site
-  </div>
+  <section>
+    <Paper>
+      <Container fixed>
+        <Box className="header__logo-title" mt={10}>
+          Product App
+        </Box>
+      </Container>
+    </Paper>
+  </section>
 );
--- src/components/ProductList/ProductList.jsx
+++ src/components/ProductList/ProductList.jsx
@@ -1,5 +1,6 @@
 /* eslint-disable max-len */
-import React from 'react';
+import React, { useState } from 'react';
+import Modal from 'react-modal';
 import PropTypes from 'prop-types';
 import { Button, Box } from '@material-ui/core';
 import { Link } from 'react-router-dom';
@@ -7,77 +8,126 @@ import like from '../../images/icon.png';
 
 import './ProductList.scss';
 
-export const ProductList = ({ products }) => (
-  <>
+export const ProductList = ({ products }) => {
+  const [isOpenModal, setOpenModal] = useState(false);
 
-    <Box mt={10} ml={5}>
-      <Button variant="contained" color="secondary">
-        <Link
-          to="/addProducts"
-          className="product__title"
-        >
-          Add new Product
-        </Link>
-      </Button>
-    </Box>
+  const style = {
+    content: {
+      borderRadius: '4px',
+      height: '400px',
+      left: '50%',
+      top: '50%',
+      transform: 'translate(-50%, -50%)',
+      padding: '2rem',
+      position: 'fixed',
+      right: 'auto',
+      width: '400px',
 
-    <Box mt={1} ml={5} display="flex" flexDirection="column">
-      <ul className="product-list">
-        {products.map((product) => (
-          <Box mr={3}>
-            <li
-              key={product.id}
-              className="product-list__product product"
-              style={{ width: '500px' }}
-            >
-              <h2
-                className="product__title"
+    },
+  };
+
+  return (
+    <>
+
+      <Box mt={10} ml={5}>
+        <Button variant="contained" color="secondary">
+          <Link
+            to="/addProducts"
+            className="product__button-add"
+          >
+            Add new Product
+          </Link>
+        </Button>
+      </Box>
+
+      <Box mt={1} ml={5} display="flex" flexDirection="column">
+        <ul className="product-list">
+          {products.map((product) => (
+            <Box mr={3}>
+              <li
+                key={product.id}
+                className="product-list__product product"
+                style={{ width: '500px' }}
               >
-                {product.name.toUpperCase()}
-              </h2>
+                <h2
+                  className="product__title"
+                >
+                  {product.name.toUpperCase()}
+                </h2>
 
-              <div>
-                <img
-                  className="product__photo"
-                  alt="iphone 12"
-                  src={product.imageUrl}
-                  style={{ height: '300px' }}
-                />
-              </div>
+                <div>
+                  <img
+                    className="product__photo"
+                    alt="iphone 12"
+                    src={product.imageUrl}
+                    style={{ height: '300px' }}
+                  />
+                </div>
 
-              <button
-                type="button"
-              >
-                Edit
-              </button>
+                <Box display="flex" justifyContent="space-between">
+                  <Button
+                    variant="contained"
+                    type="button"
+                    color="primary"
+                    onClick={() => {
+                      setOpenModal(true);
+                    }}
+                  >
+                    Edit
+                  </Button>
 
-              <p className="product__description">{product.description}</p>
+                  <Modal
+                    isOpen={isOpenModal}
+                    style={style}
+                  >
+                    <Button
+                      type="button"
+                      color="secondary"
+                      onClick={() => {
+                        setOpenModal(false);
+                      }}
+                    >
+                      X
+                    </Button>
+                  </Modal>
 
-              <p>
-                {`Count: ${product.count}`}
-              </p>
+                  <Button
+                    variant="contained"
+                    type="button"
+                    style={{ backgroundColor: '#DB4E3F', color: '#ffff' }}
+                  >
+                    Delete
+                  </Button>
+                </Box>
 
-              <p>
-                {`Color: ${product.color}`}
-              </p>
+                <p className="product__description">{product.description}</p>
 
-              <p>
-                {`Size: ${product.width} x ${product.height}`}
-              </p>
+                <p>
+                  {`Count: ${product.count}`}
+                </p>
 
-              <div className="product__container">
-                <img alt="iphone" src={like} className="product__like" />
-                <img alt="iphone" src={like} className="product__like" />
-              </div>
+                <p>
+                  {`Color: ${product.color}`}
+                </p>
 
-            </li>
-          </Box>
-        ))}
-      </ul>
-    </Box>
+                <p>
+                  {`Size: ${product.width} x ${product.height}`}
+                </p>
 
-  </>
-);
+                <div className="product__container">
+                  <img alt="iphone" src={like} className="product__like" />
+                  <img alt="iphone" src={like} className="product__like" />
+                </div>
+
+              </li>
+            </Box>
+          ))}
+        </ul>
+      </Box>
+
+    </>
+  );
+};
 
 ProductList.propTypes = PropTypes.shape({
   id: PropTypes.number.isRequired,
